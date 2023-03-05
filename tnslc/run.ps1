65793a7933de5da3c4c63bd69d32abe121f7ea30
Kyle Gunger <kgunger12@gmail.com>
2023-03-05 16:01:33 -0500

    Get more basic code gen working + bitwise and, or, xor + negate values + fix basic variable setting + fix windows based line endings

--- tnslc/run.ps1
+++ tnslc/run.ps1
@@ -1,5 +1,5 @@
 if ($args.Length -gt 0) {
-    ..\tint.exe -flags """$args""" -in tnslc.tnsl
+    ..\tint.exe -flags "$args" -in tnslc.tnsl
     nasm -f win64 -o "$($args[0]).obj" "$($args[0]).asm"
     gcc -o "$($args[0]).exe" "$($args[0]).obj"
 } else {
