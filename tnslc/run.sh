3bfdbd26958e3b8193500dd5fad8dcd58c5a8457
Kyle Gunger <kgunger12@gmail.com>
2023-08-31 01:00:44 -0400

    Updates to include new c_wrap

--- tnslc/run.sh
+++ tnslc/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+BUILD_DIR="./build"
+ARTIFACTS_DIR="$BUILD_DIR/artifacts"
+
+if [ ! -d $BUILD_DIR ]; then
+	mkdir $BUILD_DIR
+fi
+
+if [ ! -d $ARTIFACTS_DIR ]; then
+	mkdir $ARTIFACTS_DIR
+fi
+
 ../tint -in tnslc.tnsl -flags "$1"
+mv -f "$1.asm" "$ARTIFACTS_DIR/$1.asm"
+nasm -f elf64 -o "$ARTIFACTS_DIR/$1.o" "$ARTIFACTS_DIR/$1.asm"
+clang -o "./build/${1%.*}" "$ARTIFACTS_DIR/$1.o"
+
