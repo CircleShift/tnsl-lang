61e1e5ce377719c8e9e437e5ba79ba06fc1de4ba
Kyle Gunger <kgunger12@gmail.com>
2024-12-13 16:10:14 -0500

    Type generation for compile modules

--- tnslc/build.sh
+++ tnslc/build.sh
@@ -5,7 +5,7 @@ ARTIFACT_DIR=$BUILD_DIR/artifacts
 
 mkdir -p $BUILD_DIR
 mkdir -p $ARTIFACT_DIR
-filename=tnslc.tnsl
+filename=$1
 filename="${filename%.*}"
 ./ctc $filename.tnsl $ARTIFACT_DIR/$filename.asm
 nasm -g -f elf64 -o $ARTIFACT_DIR/$filename.o $ARTIFACT_DIR/$filename.asm
