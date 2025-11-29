51854e5db46033712b5dbbf78d769ea500eca14f
Kai Gunger <kgunger12@gmail.com>
2025-11-28 23:54:11 -0500

    Update module building code

--- tnslc/build.sh
+++ tnslc/build.sh
@@ -7,7 +7,18 @@ mkdir -p $BUILD_DIR
 mkdir -p $ARTIFACT_DIR
 filename=$1
 filename="${filename%.*}"
+
 ./ctc $filename.tnsl $ARTIFACT_DIR/$filename.asm
+
+if [ $? -ne 0 ]; then
+	exit $?
+fi
+
 nasm -g -f elf64 -o $ARTIFACT_DIR/$filename.o $ARTIFACT_DIR/$filename.asm
+
+if [ $? -ne 0 ]; then
+	exit $?
+fi
+
 gcc -ggdb -o $BUILD_DIR/$filename $ARTIFACT_DIR/$filename.o
 
